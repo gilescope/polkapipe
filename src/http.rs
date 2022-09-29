d0988725ef293baf2b9090ddcacec714f9611fd6
gilescope <gilescope@gmail.com>
2022-09-29 09:16:39 +0100

    update http-web

--- src/http.rs
+++ src/http.rs
@@ -2,11 +2,9 @@ use crate::prelude::*;
 use async_trait::async_trait;
 use core::{convert::TryInto, fmt};
 use jsonrpc::{
-	error::{result_to_response, standard_error, RpcError, StandardError},
-	serde_json,
-	serde_json::{to_string, value::to_raw_value},
+	error::{standard_error, StandardError},
+	serde_json::value::to_raw_value,
 };
-use serde_json::value::RawValue;
 pub use surf::Url;
 
 use crate::rpc::{self, Rpc, RpcResult};
@@ -24,20 +22,17 @@ impl Backend {
 	}
 }
 
-#[async_trait]
+#[cfg_attr(target_arch = "wasm32", async_trait(?Send))]
+#[cfg_attr(not(target_arch = "wasm32"), async_trait)]
 impl Rpc for Backend {
-	/// HTTP based JSONRpc request expecting an hex encoded result
-	async fn rpc(&self, method: &str, params: Vec<Box<RawValue>>) -> RpcResult {
+	/// HTTP based JSON RPC request expecting valid json result.
+	async fn rpc(&self, method: &str, params: &str) -> RpcResult {
 		log::info!("RPC `{}` to {}", method, &self.0);
-		let req = surf::post(&self.0).content_type("application/json").body(
-			to_string(&rpc::Request {
-				id: 1.into(),
-				jsonrpc: Some("2.0"),
-				method,
-				params: &params,
-			})
-			.unwrap(),
-		);
+		let id = 1_u32;
+		let req = surf::post(&self.0).content_type("application/json").body(format!(
+			"{{\"id\":{}, \"jsonrpc\": \"2.0\", \"method\":\"{}\", \"params\":{}}}",
+			id, method, params
+		));
 		let client = surf::client().with(surf::middleware::Redirect::new(2));
 		let mut res = client
 			.send(req)
@@ -46,15 +41,12 @@ impl Rpc for Backend {
 
 		let status = res.status();
 		let res = if status.is_success() {
-			res.body_json::<rpc::Response>()
-				.await
-				.map_err(|err| {
-					standard_error(
-						StandardError::ParseError,
-						Some(to_raw_value(&err.to_string()).unwrap()),
-					)
-				})?
-				.result::<String>()?
+			res.body_json::<rpc::Response>().await.map_err(|err| {
+				standard_error(
+					StandardError::ParseError,
+					Some(to_raw_value(&err.to_string()).unwrap()),
+				)
+			})?
 		} else {
 			log::debug!("RPC HTTP status: {}", res.status());
 			let err = res.body_string().await.unwrap_or_else(|_| status.canonical_reason().into());
@@ -67,10 +59,10 @@ impl Rpc for Backend {
 			})
 		};
 
-		log::debug!("RPC Response: {}...", &res[..res.len().min(20)]);
+		// log::debug!("RPC Response: {}...", &res[..res.len().min(20)]);
 		// assume the response is a hex encoded string starting with "0x"
-		let response = hex::decode(&res[2..])
-			.map_err(|_err| standard_error(StandardError::InternalError, None))?;
-		Ok(response)
+		// let response = hex::decode(&res[2..])
+		// 	.map_err(|_err| standard_error(StandardError::InternalError, None))?;
+		Ok(res.result.unwrap())
 	}
 }
