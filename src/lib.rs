8bc50fcfd00f727292ac2038f858c32101526652
gilescope <gilescope@gmail.com>
2023-03-25 15:35:10 +0000

    remove dead code

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 #![allow(incomplete_features)]
 #![feature(async_fn_in_trait)]
+// #![feature(return_position_impl_trait_in_trait)]
 /*!
 Polkapipe is a fork of Sube that has few deps with multi-backend support
 that can be used to access substrate based chains. It leaves encoding / decoding
@@ -66,96 +67,6 @@ pub mod smoldot_std;
 pub mod ws_web;
 
 mod rpc;
-
-/// Trait to enable send sync bounds only for non-wasm.
-// #[cfg(target_family = "wasm")]
-// pub trait BackendParent {}
-
-// #[cfg(not(target_family = "wasm"))]
-// pub trait BackendParent: Send + Sync {}
-
-/// Generic definition of a blockchain backend
-///
-/// ```rust,ignore
-/// #[async_trait]
-/// pub trait Backend {
-///     async fn query_bytes(&self, key: &StorageKey) -> Result<Vec<u8>>;
-///
-///     async fn submit<T>(&self, ext: T) -> Result<()>
-///     where
-///         T: AsRef<[u8]> + Send;
-///
-///     async fn metadata(&self) -> Result<Metadata>;
-/// }
-/// ```
-// #[cfg_attr(target_arch = "wasm32", async_trait(?Send))]
-// #[cfg_attr(not(target_arch = "wasm32"), async_trait)]
-// pub trait Backend: BackendParent {
-// 	/// Get raw storage items form the blockchain
-// 	async fn query_storage(&self, key: &[u8], as_of: Option<&[u8]>) -> crate::Result<Vec<u8>>;
-
-// 	/// Get block hash for block number
-// 	async fn query_block_hash(&self, block_number: &[u32]) -> crate::Result<Vec<u8>>;
-
-// 	/// Get block for block hash
-// 	async fn query_block(
-// 		&self,
-// 		block_hash: Option<&str>,
-// 	) -> crate::Result<serde_json::value::Value>;
-
-// 	/// Send a signed extrinsic to the blockchain
-// 	async fn submit<T>(&self, ext: impl AsRef<[u8]> + Send) -> Result<()>;
-
-// 	/// TODO: this can be achieved with query_state_call
-// 	async fn query_metadata(&self, as_of: Option<&[u8]>) -> crate::Result<Vec<u8>>;
-
-// 	/// E.g. method = "Metadata_metadata" format: CamelCase_snake_case
-// 	async fn query_state_call(&self, method: &str, key: &[u8], as_of: Option<&[u8]>) ->
-// crate::Result<Vec<u8>>; }
-
-/// A Dummy backend for offline querying of metadata
-// pub struct Offline(pub Vec<u8>);
-
-// impl BackendParent for Offline {}
-
-// #[cfg_attr(target_arch = "wasm32", async_trait(?Send))]
-// #[cfg_attr(not(target_arch = "wasm32"), async_trait)]
-// impl Backend for Offline {
-// 	async fn query_storage(&self, _key: &[u8], _as_of: Option<&[u8]>) -> Result<Vec<u8>> {
-// 		Err(Error::ChainUnavailable)
-// 	}
-
-// 	// /// Send a signed extrinsic to the blockchain
-// 	// async fn submit<T>(&self, _ext: T) -> Result<()>
-// 	// where
-// 	//     T: AsRef<[u8]> + Send,
-// 	// {
-// 	//     Err(Error::ChainUnavailable)
-// 	// }
-
-// 	async fn query_block_hash(&self, _block_number: &[u32]) -> crate::Result<Vec<u8>> {
-// 		Err(Error::ChainUnavailable)
-// 	}
-
-// 	async fn query_block(
-// 		&self,
-// 		_block_hash: Option<&str>,
-// 	) -> crate::Result<serde_json::value::Value> {
-// 		Err(Error::ChainUnavailable)
-// 	}
-
-// 	async fn query_metadata(&self, _as_of: Option<&[u8]>) -> Result<Vec<u8>> {
-// 		Ok(self.0.clone())
-// 	}
-
-// 	async fn query_state_call(&self, _method: &str, _key: &[u8], _as_of: Option<&[u8]>) ->
-// Result<Vec<u8>> { 		Err(Error::ChainUnavailable)
-// 	}
-
-// 	async fn submit<T>(&self, _ext: impl AsRef<[u8]> + Send) -> Result<()> {
-//         unimplemented!("Can't send in offline mode")
-//     }
-// }
 
 #[derive(Clone, Debug)]
 pub enum Error {
